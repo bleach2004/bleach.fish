9a3973519016136d3c9bef437562fb7232bb714a
bleach <83350763+bleach2004@users.noreply.github.com>
2024-11-11 12:03:42 +0000

    placeholder commit

---
+++ bleach.fish/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Page1 from  './Page1.tsx'
+
+function App() {
+
+  return (
+    <>
+    <div className='w-screen items-center'>
+      <Page1 />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ bleach.fish/src/Page1.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Page1: React.FC = () => {
+  return (
+    <div style={styles.container}>
+      <img src="src/assets/page1/1.png" style={styles.image} />
+      <img src="src/assets/page1/2.png" style={styles.image} />
+      <img src="src/assets/page1/1.png" className='scale-x-[-1]' style={styles.image} />
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '20px', // Space between images
+    marginTop: '20px', // Optional top margin
+  } as React.CSSProperties,
+  image: {
+    width: '200px', // Adjust to desired size
+    height: 'auto',
+    borderRadius: '8px', // Optional rounded corners
+  } as React.CSSProperties,
+};
+
+export default Page1;
