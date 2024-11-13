ca57f874b595a0e145729129a01654b51e03274c
bleach <nzm@s020.net>
2024-11-13 21:06:26 +0100

    bleach's corner
    added stylized front page menu

--- bleach.fish/src/Page1.tsx
+++ bleach.fish/src/Page1.tsx
@@ -2,26 +2,19 @@ import React from 'react';
 
 const Page1: React.FC = () => {
   return (
-    <div style={styles.container}>
-      <img src="/page1/1.png" style={styles.image} />
-      {/* <img src="./src/assets/page1/2.png" style={styles.image} />
-      <img src="./src/assets/page1/1.png" className='scale-x-[-1]' style={styles.image} /> */}
+    <div className='skin'>
+      <div className='meat'>
+        <p>BLEACH'S CORNER</p><br />
+        <a>AUDIO</a>
+        <a>VISUAL</a>
+        <a>SCRAPYARD</a>
+        <a>BLOG</a>
+        <a>CAVERN</a>
+      </div>
+      <img src='page1/1.png' className='chess'></img>
     </div>
-  );
-};
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    gap: '20px', // Space between images
-    marginTop: '20px', // Optional top margin
-  } as React.CSSProperties,
-  image: {
-    width: '200px', // Adjust to desired size
-    height: 'auto',
-    borderRadius: '8px', // Optional rounded corners
-  } as React.CSSProperties,
+  );
 };
 
 export default Page1;
