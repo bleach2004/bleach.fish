a32a7540ce9f5cc67f86b15c9dfa9ed8c591b737
bleach <83350763+bleach2004@users.noreply.github.com>
2024-12-18 07:09:21 +0000

    center the song page on mobile

--- bleach.fish/src/Home.tsx
+++ bleach.fish/src/Home.tsx
@@ -3,17 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
-    <div className="skin vert-center-wrapper">
+    <div className="skin">
       <div className="meat text-center">
         <p className='h1'>bleach's corner</p>
         <Link to="/audio" className='p text-white'>audio</Link>
-        {/* <Link to="/visual">VISUAL</Link>
-        <Link to="/scrapyard">SCRAPYARD</Link> */}
         <Link to="/blog" className='p  text-white'>blog</Link>
-        {/* <Link to="/cavern">CAVERN</Link> */}
       </div>
-      {/* Uncomment the image below if needed */}
-      {/* <img src='page1/1.png' className='chess' alt='Chess'></img> */}
     </div>
   );
 };
